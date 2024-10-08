722fb91325744f89bee79aebbbf7b9b51fb4b1b5
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-10-08 18:42:21 +0300

    initial commit

---
+++ src/main/java/catshelter/catshelterapp/CatShelterAppApplication.java
@@ -0,0 +1,13 @@
+package catshelter.catshelterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatShelterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatShelterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/catshelter/catshelterapp/CatShelterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package catshelter.catshelterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatShelterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
