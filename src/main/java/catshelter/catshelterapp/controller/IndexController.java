a147241712281145cfab5ec653a34627538a3960
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-10-22 22:23:11 +0300

    html

--- src/main/java/catshelter/catshelterapp/controller/IndexController.java
+++ src/main/java/catshelter/catshelterapp/controller/IndexController.java
@@ -29,11 +29,9 @@ public class IndexController {
     public String cats() {
         return "cats";
     }
-
     public String login(Model model) {
         return "login";
     }
-
     public String loginError() {
         return "login-error";
     }
